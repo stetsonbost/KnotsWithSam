2ada1e5baa5bff4f81c13865ac3b55f6428da3f5
stetsonbost <sbost@hmc.edu>
2016-05-21 14:21:36 -0700

    Deal with expected vs actual type errors

--- Invariants.hs
+++ Invariants.hs
@@ -17,7 +17,7 @@ For classical crossings,
   x2 = numeric label of the arrowtail
   x3 = component number of the arrowtail
   x4 = numeric label of the arrowhead
-For saddles, (which don't have arrow heads)
+For saddles, (which don't have arrowheads)
   x1
 
 To compile, use Makefile (type "make" at command line) or type at command line
@@ -27,10 +27,31 @@ To run, type at command line
         ./invariants <file_with_gauss_codes>
 -}
 
--- | TODO: EVERYTHING
-
-
 purpose = "Given a gauss code for a knotted surface,\
       \this program will produce some (which?) of its invariants."
 
-main = putStrLn purpose
+
+-- | This is the example in Sam's diagrams.pdf
+test1 = [[2,1,1,2,10],[ 1,2,9,1,2],[-2,1,3,2,8],[-1,2,7,1,4],[-1,2,5,2,6]]
+
+-- | TODO: find another example to play with.
+--let test2 = [[]]
+
+-- | TODO: work out our idea by hand for a small example.
+-- |    This will greatly help with the implementation of this
+
+-- | Calculates the subdiagrams as powersets
+getSubdiagrams :: [[a]] -> [[a]]
+getSubdiagrams [[first]] = 
+  [[first]]
+getSubdiagrams [[first],rest] = [[first]]
+
+main :: IO ()
+main = do print "hi"
+  --print purpose ++ "We'll eventually take in commandline args"
+
+
+--let orthogonalSet = gramScmidt input
+--let subdiagrams = getSubdiagrams orthogonalSet
+--putStrLn "subdiagrams:\n " ++ subdiagrams
+
--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-invariants :
+invariants : Invariants.hs
 	ghc -o invariants Invariants.hs 
 clean :
 	rm invariants Invariants.o Invariants.hi
