12c1e8754cf0bf8f20a582d0bb75c7fff4ee3ae0
stetsonbost <sbost@hmc.edu>
2016-07-11 12:00:45 -0700

    Improve equiv function

--- FiniteType.py
+++ FiniteType.py
@@ -43,32 +43,48 @@
           G = [(x0,x1,x2,x3,x4), (y0,y1,y2,y3,y4), ...].
 
     Now that we have a labelling for the components of the diagram, we can
-    construct a tuple C representing the components of G. Suppose G has
+    construct a dictionary C representing the components of G. Suppose G has
     components labelled 'A', 'B', ..., and the components have crossings a1,
-    a2, ... for 'A', then b1, b2, ... for 'B', and so on. Then we can represent
-    the components as
-          C = (('A', [a1,a2,...]), ('B', [b1,b2,...]), ...).
+    a2, ..., am for 'A', then b1, b2, ..., bn for 'B', and so on. Then we can
+    represent the components as
+          C = {'A':[a1,a2,...,am], 'B':[b1,b2,...,bn], ...}.
 """
 
 import sys  # used in main to read command line arguments
 import ast  # used in main to convert string to list
 from copy import deepcopy # used in calculateSubdiagrams
+from subprocess import call # used to execute terminal commands
 
-def calculateSubdiagrams(subdiagrams, diagram):
+def calculateSubdiagrams(subdiagrams, diagram, components):
   """
   Returns set of subdiagrams whose sum is diagram
   """
-  # Add diagram (as tuple) to set of subdiagrams 
-  subdiagrams.add(tuple(diagram))
+  
+  for piece in components.items():
+    piece = tuple(piece[1])
+
+  componentList = []
+  for comp in components.items():
+    # create a list whose first element is the component name
+    piece = [comp[0]]
+    # append the list of crossings
+    piece.append(tuple(comp[1]))
+    componentList.append(tuple(piece))
+
+  # Add diagram (as tuple) to set of subdiagrams with components
+  subdiagrams.add((tuple(diagram), tuple(componentList)))
 
   # Go through the crossings of the diagram
   for crossing in diagram:
-    # Make a deep copy of the diagram
+    # Make a deep copy of the diagram and components
     copyDiagram = deepcopy(diagram)
-    # Remove a crossing from the code
+    copyComponents = deepcopy(components)
+    # Remove a crossing from the code and components
     copyDiagram.remove(crossing)
+    copyComponents[crossing[1]].remove(crossing[2])
+    copyComponents[crossing[3]].remove(crossing[4])
     # Recursively find remaining subdiagrams
-    subdiagram = calculateSubdiagrams(subdiagrams, copyDiagram)
+    subdiagram = calculateSubdiagrams(subdiagrams, copyDiagram, copyComponents)
     # Add new subdiagrams to the set of all subdiagrams
     subdiagrams.union(subdiagram)
 
@@ -79,7 +95,7 @@ def obtainLinearCombination(linearComboString):
   print "TODO"
   return None
 
-def innerProduct(linearCombo1, linearCombo2):
+def innerProd(linearCombo1, linearCombo2):
   """
   Returns the inner product of 2 linear combinations of subdiagrams.
   The arguments linearCombo1 and linearCombo2 must be linear combinatons 
@@ -95,8 +111,6 @@ def innerProduct(linearCombo1, linearCombo2):
   linearComboCopy1 = deepcopy(linearCombo1)
   linearComboCopy2 = deepcopy(linearCombo2)
   print len(linearComboCopy2[0][1])
-  # print "linearComboCopy1\t" + str(linearComboCopy1)
-  # print "linearComboCopy2\t" + str(linearComboCopy2)
 
   # get list of subdiagrams, get coefficients
   for coeffAndSubdiagrams1 in linearComboCopy1:
@@ -109,15 +123,9 @@ def innerProduct(linearCombo1, linearCombo2):
       coeff2 = coeffAndSubdiagrams2[0]
       subdiagramsToRemove2 = []
 
-      # print "subSet 1\t" + str(subdiagramSet1)
-      # print "subSet 2\t" + str(subdiagramSet2)
-
       # look for equivalent pairs of dashed subdiagrams in both lists
       for subdiagram1 in subdiagramSet1:
-        for subdiagram2 in subdiagramSet2:
-          # print "sub 1\t" + str(subdiagram1)
-          
-
+        for subdiagram2 in subdiagramSet2:          
           # for equivalent pairs of dashed subdiagrams, increment innerProd by 
           #   product of coefficients and remove the subdiagrams to avoid 
           #   over-counting diagrams
@@ -125,7 +133,7 @@ def innerProduct(linearCombo1, linearCombo2):
             innerProd += coeff1 * coeff2
             subdiagramsToRemove1.append(subdiagram1)
             subdiagramsToRemove2.append(subdiagram2)
-
+          print "  ", innerProd
         # remove the used subdiagrams
         # print "sub 2\t" + str(subdiagramSet2)
         # print " 2 " + str(subdiagramsToRemove2)
@@ -153,25 +161,33 @@ def getPrev(crossing):
 def equiv(subdiagram1, subdiagram2):
   """
   Returns True if both dashed subdiagrams are equivalent
-    i.e. (0,A,1,A,3) is equivalent to (0,X,2,X,3)
+    i.e. (0,'A',1,'A',3) is equivalent to (0,'X',2,'X',3),
   Returns False otherwise
 
   Arguments must be a tuple of crossings representing a single subdiagram
   """
   # TODO: Make sure arguments are the right type
-  # TODO: Make this work for subdiagrams of length > 1
+  # TODO: Make this work for subdiagrams of length >= 1
   # subdiagrams are not equivalent if they have different numbers of crossings
-  if len(subdiagram1) != len(subdiagram2):
+  # print "sub1\t", subdiagram1, len(subdiagram1[0])
+  # print "sub2\t", subdiagram2, len(subdiagram2[0])
+  if len(subdiagram1[0]) != len(subdiagram2[0]):
     return False
   # look for a match
-  for crossing1 in subdiagram1:
-    match = False
-    for crossing2 in subdiagram2:
+  for i in range(len(subdiagram1[0])-1):
+    crossing1 = subdiagram1[0][i]
+    typeMatch = False
+    for j in range(len(subdiagram2[0])-1):
+      crossing2 = subdiagram2[0][j]
+      print "\tc1 ",crossing1
+      print "\tc2 ",crossing2
       # check for same crossing type
-      if crossing1[0] == crossing2[0]:
-        match = True
-      if match == False:
-        return False
+      # TODO: check for empty crossing
+      if len(crossing1) == 5 and len(crossing2) == 5:
+        if crossing1[0] == crossing2[0]:
+          print  "              :)"
+          typeMatch = True
+        
 
   return True
 
@@ -197,37 +213,53 @@ def main():
   # Read input file with linear combination of diagrams
   #   This should be a list of tuples (a,D,C) where
   #   a is an integer coefficient and
-  #   D is the diagram and
-  #   C is the tuple of components with an ordered list of their crossings
+  #   D is the diagram (as a list of 5-tuples) and
+  #   C is the dict of components with an ordered list of their crossings
   linearComboString = open(sys.argv[1]).read()
   linearCombo = ast.literal_eval(linearComboString) 
-  # linearCombo is list of tuples of form (int, list of 5-tuples of ints)
+  # linearCombo is list of tuples of form 
+  #    (integer, list of 5-tuples, dictionary of components)
 
-  # TODO: Check that input file is valid
+  # TODO: Check that input file is valid, meaning it is in the format specified
+  #   above and doesn't have duplicate crossings
 
   # Expand each diagram (solid arrows/chords) as sum of its
   #   subdiagrams (dashed arrows/chords).
   #   Subdiagrams sum will be Set or list of 5-tuples.
   # Rewrite the linear combination using the subdiagrams.
 
-  linearComboSubdiagrams = []
+  linComboSubdiagrams = []
   for diagram in linearCombo:
-    subdiagrams = calculateSubdiagrams(set(), diagram[1])
-    linearComboSubdiagrams.append((diagram[0], subdiagrams))
-  # linearComboSubdiagrams is a
+    subdiagrams = calculateSubdiagrams(set(), diagram[1], diagram[2])
+    linComboSubdiagrams.append((diagram[0], subdiagrams))
+  # linComboSubdiagrams is a
   #   List of tuples            (<-- linear combinations)
   #     of integer and set      (<-- integer = coefficient, set = subdiagrams)
   #       of tuples             (<-- tuple = dashed subdiagrams)
   #         of tuples           (<-- tuple = crossing)
   #           of the form explained above (5-tuples)
-  # print linearComboSubdiagrams
+  # print linComboSubdiagrams
 
   # TODO: Pick sets of subdiagrams
   # TODO: Compute inner product of 2 diagrams using their sums
   #   of subdiagrams.
-  # innerProd = innerProduct(linearCombo1, linearCombo2)
-  innerProdTest = innerProduct([linearComboSubdiagrams[0]], [linearComboSubdiagrams[1]])
-  print innerProdTest
+  # innerProdTest = innerProd([linComboSubdiagrams[0]], [linComboSubdiagrams[1]])
+  # print innerProdTest, "\n"
+
+  # testing equiv
+
+  sub1 =  (((0, 'A', 1, 'B', 10), (0, 'A', 3, 'B', 8), (-1, 'B', 7, 'A', 4)), (('A', (1, 3, 4)), ('B', (7, 8, 10))))
+  sub2 =  (((0, 'A', 1, 'B', 10), (1, 'B', 9, 'A', 2), (0, 'A', 3, 'B', 8)),  (('A', (1, 2, 3)), ('B', (8, 9, 10))))
+  sub3 =  (((0, 'A', 1, 'B', 10), (0, 'A', 3, 'B', 8), (1, 'B', 9, 'A', 2)),  (('A', (1, 2, 3)), ('B', (8, 9, 10))))
+  sub4 =  ((), (('A', ()), ('B', ())))
+  sub5 =  ((), (('C', ()), ('D', ())))
+  sub6 =  (((0, 'A', 1, 'B', 10), (0, 'A', 3, 'B', 8), (-1, 'B', 7, 'A', 4)), (('A', (1, 3, 4)), ('B', (7, 8, 10))))
+
+  print equiv(sub1,sub1) # true
+  print equiv(sub1,sub2) # false
+  print equiv(sub2,sub3) # true
+  print equiv(sub1,sub4) # false
+  print equiv(sub4,sub5) # true
 
   # TODO: Generate Yoshikawa submodule generators for n arrows
   #   sys.argv[2] is the number of arrows/chords
