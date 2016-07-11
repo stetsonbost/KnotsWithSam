9850a6f91820695faa7e69c502b7291c099538e3
stetsonbost <sbost@hmc.edu>
2016-07-11 12:32:46 -0700

    Update Ben's code for yoshikawa submodules

---
+++ yoshikawa_code/move1.hs
@@ -0,0 +1,33 @@
+import ListMaker
+
+main = do
+	putStrLn "LHS of n = 1\n"
+	print $ left 1
+	putStrLn "\n\n"
+	putStrLn "RHS of n = 1\n"
+	print $ right 1
+	putStrLn "\n\n"
+	putStrLn "LHS of n = 2\n"
+	print $ left 2
+	putStrLn "\n\n"
+	putStrLn "RHS of n = 2\n"
+	print $ right 2
+	putStrLn "\n\n"
+	putStrLn "LHS of n = 3\n"
+	print $ left 3
+	putStrLn "\n\n"
+	putStrLn "RHS of n = 3\n"
+	print $ right 3
+	putStrLn "\n\n"
+	putStrLn "LHS of n = 4\n"
+	print $ left 4
+	putStrLn "\n\n"
+	putStrLn "RHS of n = 4\n"
+	print $ right 4
+
+
+
+
+left n = move1 n
+right n = map (\x -> tail x) $ move1 n
+
